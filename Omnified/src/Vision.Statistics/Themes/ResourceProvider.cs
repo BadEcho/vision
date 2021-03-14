8859d5bb575bd90be88a4a3a86d2b2e42a9c9ed5
Matt Weber <matt.weber@mts.com>
2021-03-14 01:11:24 -0600

    Normalizing Vision.Statistics to use the proper resource layout.
    Added an IResourceProvider for Vision.Statistics.

---
+++ Omnified/src/Vision.Statistics/Themes/ResourceProvider.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright>
+//      Created by Matt Weber <matt@badecho.com>
+//      Copyright @ 2021 Bad Echo LLC. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+using System.Composition;
+using BadEcho.Fenestra;
+
+namespace BadEcho.Omnified.Vision.Statistics.Themes
+{
+    /// <summary>
+    /// Provides access to resources belonging to the Statistics plugin for Vision.
+    /// </summary>
+    [Export(typeof(IResourceProvider))]
+    internal sealed class ResourceProvider : IResourceProvider
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResourceProvider"/> class.
+        /// </summary>
+        public ResourceProvider()
+        {
+            ResourceUri = PackUri.FromRelativePath<ResourceProvider>("Root.xaml");
+        }
+
+        /// <inheritdoc/>
+        public Uri ResourceUri 
+        { get; }
+    }
+}
